944b63f0cfb82e8689ecf9bfb6e59d09d83e7f67
Tim Vaughan <tgvaughan@gmail.com>
2018-03-23 15:06:50 +0100

    Added missing files.

---
+++ lecture1/plot_treecount.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    var target = document.getElementById("treecount");
+    
+    var data = {x: [], y: [], showlegend: false};
+    var acc = 0;
+    for (var m=3; m<=100; m+=1) {
+        acc = acc + Math.log10(2*m-3)
+        data.x.push(m);
+        data.y.push(acc);
+    }
+
+    var guide = {x: [0,100], y:[100,100], mode: "lines", name: "Particles in universe", showlegend: true};
+
+    var layout =  {
+        width: 900,
+        height: 400,
+        margin: {
+            t:10
+        },
+        xaxis: {
+            title: "Leaf/sequence count",
+        },
+        yaxis: {
+            title: "log<sub>10</sub>(Number of topologies)"
+        },
+    };
+
+
+    Plotly.plot("treecount", [data, guide], layout, {displayModeBar: false});
+});
