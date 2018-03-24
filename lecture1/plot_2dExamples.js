444235e5b43e1dabecc4a7453d0bc4284a716880
Tim Vaughan <tgvaughan@gmail.com>
2018-03-24 23:53:43 +0100

    Added 2d mcmc example.

--- lecture1/plot_2dExamples.js
+++ lecture1/plot_2dExamples.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     function targetDensity(x,y) {
         var sx = 1, sy = 1;
         var mx = 0, my = 0;
-        var rho = 0.9;
+        var rho = 0.95;
         var density = 1/(2*Math.PI*sx*sy*Math.sqrt(1-rho*rho)) *
             Math.exp(-1/(2*(1-rho*rho))*(Math.pow(x-mx,2)/(sx*sx)
                                          + Math.pow(y-my,2)/(sy*sy)
@@ -12,17 +12,19 @@ $(document).ready(function() {
         return density;
     }
 
+    var lower=-3, upper=3;
+
     xvec = [];
     yvec = [];
-    for (var xy=-2; xy<=2; xy+=0.05) {
+    for (var xy=lower; xy<=upper; xy+=0.05) {
         xvec.push(xy);
         yvec.push(xy);
     }
         
     zmat = [];
-    for (var x=-2; x<=2; x+=0.05) {
+    for (var x=lower; x<=upper; x+=0.05) {
         var row = [];
-        for (var y=-2; y<=2; y+= 0.05) {
+        for (var y=lower; y<=upper; y+= 0.05) {
             row.push(targetDensity(x,y));
         }
         zmat.push(row);
@@ -39,14 +41,84 @@ $(document).ready(function() {
 
     var layout =  {
         width: 700,
-        height: 600,
+        height: 400,
         margin: {
-            t:10
+            t:10,b:40
         },
-        xaxis: {title: "x"},
-        yaxis: {title: "y"},
+        xaxis: {title: "x1"},
+        yaxis: {title: "x2"},
     };
 
-
     Plotly.newPlot('targetDensity2d', [truth], layout, {displayModeBar: false});
+
+
+    // MCMC
+    function doMCMC(targetDensity, proposal, N, x0, y0) {
+
+
+        var x = [x0];
+        var y = [y0];
+        var currentP = targetDensity(x0, y0);
+
+        for (var iter=1; iter<N; iter+=1) {
+            prime = proposal(x[iter-1], y[iter-1])
+
+            var newP = targetDensity(prime.x, prime.y);
+
+            var alpha = newP/currentP;
+
+            if (alpha>1 || Math.random()<alpha) {
+                x.push(prime.x)
+                y.push(prime.y)
+                currentP = newP;
+            } else {
+                x.push(x[iter-1]);
+                y.push(y[iter-1]);
+            }
+        }
+
+        return({x: x, y: y,
+                mode: 'markers+lines',
+                type: 'scatter'})
+    }
+
+    function makeProposal(w) {
+        return function(x, y) {
+            var xp, yp;
+
+            if (Math.random()<0.5) {
+                xp = x + w*(Math.random()-0.5);
+                yp = y;
+            } else {
+                xp = x;
+                yp = y + w*(Math.random()-0.5);
+            }
+
+            return {x: xp, y: yp}
+        }
+    }
+
+    var mcmcTrace = doMCMC(targetDensity, makeProposal(1), 1000, 2,-2);
+
+    var traceLayout =  {
+        width: 800,
+        height: 600,
+        margin: {
+            t:10,b:40
+        },
+        xaxis: {title: "x1"},
+        yaxis: {title: "x2"},
+    };
+
+    // Need to create new object here for some reason
+    truth = {
+        x: xvec,
+        y: yvec,
+        z: zmat,
+        type: 'contour',
+        contours: {coloring: 'heatmap'},
+        showscale: false
+    }
+
+    Plotly.plot("mcmcTrace2dFirstTry", [truth, mcmcTrace], traceLayout, {displayModeBar: false});
 });
